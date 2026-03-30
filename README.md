c4b8e63193afa9076e2df0bd4a2b88694bb54426
barrosjunior26 <barrosjunior26@outlook.com.br>
2026-03-29 21:35:09 -0300

    Inclusão do arquivo readme ao projeto.

---
+++ README.md
@@ -0,0 +1,3 @@
+## ➕ Descrição do Aplicativo
+
+Este aplicativo em C# realiza a **soma de dois valores**, garantindo que o usuário informe apenas números válidos. Ele utiliza `double.TryParse()` para validar a entrada e evitar erros, exibindo mensagens de correção sempre que necessário. Ao final, o programa mostra o resultado da soma com duas casas decimais.
--- obj/Debug/net5.0/ex035.AssemblyInfo.cs
+++ obj/Debug/net5.0/ex035.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex035")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb3b49e2c9efa44a765aa9f9fe2de1d04d15a1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex035")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex035")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
