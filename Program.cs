2eb3b49e2c9efa44a765aa9f9fe2de1d04d15a1c
barrosjunior26 <barrosjunior26@outlook.com.br>
2026-03-29 21:30:22 -0300

    Primeiro commit do app

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ex035
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis locais
+            double valor1, valor2;
+
+            Console.Clear();
+
+            Console.Write("Informe o primeiro valor: ");
+            while (!double.TryParse(Console.ReadLine(), out valor1))
+            {
+                Console.Clear();
+                Console.Write("\nValor incorreto. Por favor, tente novamente.\nInforme o primeiro valor: ");
+            }
+
+            Console.Write("Informe o segundo valor: ");
+            while (!double.TryParse(Console.ReadLine(), out valor2))
+            {
+                Console.Clear();
+                Console.Write("\nValor incorreto. Por favor, tente novamente.\nInforme o segundo valor: ");
+            }
+
+            Console.WriteLine($"O resultado da soma entre {valor1} + {valor2} = {Math.Round(valor1 + valor2, 2)}");
+        }
+    }
+}
